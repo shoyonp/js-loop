8d847553345abd7745803677b8904e4a4682781e
shoyonp <shoyonkumar143@gmail.com>
2024-09-01 22:53:32 +0600

    added basic loop

---
+++ for-of.js
@@ -0,0 +1,19 @@
+var numbers = [121, 46, 63, 386, 62, 43];
+
+console.log(numbers);
+
+// for(const num of numbers){
+//     console.log(num);
+// }
+
+const fruits = ['orange', 'apple', 'banana', 'jackfruit'];
+
+// for(){
+
+// }
+
+
+for (const fruit of fruits){
+    console.log(fruit);
+    console.log('i want to eat');
+}
---
+++ while.js
@@ -0,0 +1,5 @@
+const number = 7;
+if(number < 10){
+    console.log('i got a number')
+}
+console.log('somthing new is cooking');
