dc32355d958aee1ec1a9ce71a540168976f8ee9e
shoyonp <shoyonkumar143@gmail.com>
2024-09-02 01:27:58 +0600

    added for even file

---
+++ even.js
@@ -0,0 +1,17 @@
+// let num = 1;
+// while (num <= 10){
+//     console.log(num);
+
+//     if(num % 2 === 0){
+//         console.log('even number', num)
+//     }
+
+//     num++;
+// }
+
+let num = 0;
+let msg = 'you are my punu';
+while(num <= 60){
+    console.log(msg);
+    num++;
+}
---
+++ for.js
@@ -0,0 +1,21 @@
+/**
+ * for( ;  ;  ){
+ * }
+ * 
+ * for(lop variable declaration ; loop condition)
+ * 
+ */
+
+// for(first part ; second part ; third part){
+//     loop statement
+// }
+
+// let num = 0; // loop variable 
+// while(num , 5) { // loop condition
+//     console.log(num); // loop statement
+//     num++; // loop variable change
+// }
+
+for(let num = 0; num < 5; num++){
+    console.log(num);
+}
