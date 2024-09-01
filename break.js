3aceb70932868b8c72662ec2e8af2d3f2097b7e1
shoyonp <shoyonkumar143@gmail.com>
2024-09-02 02:57:21 +0600

    break cintinue  added

---
+++ break.js
@@ -0,0 +1,17 @@
+for(let i = 0; i< 15; i++){
+    console.log(i);
+    if(i >=5){
+        break;
+    }
+}
+
+console.log('life without you')
+
+let n = 46;
+while( n > 33){
+    if(n < 40){
+        break;
+    }
+    console.log(n);
+    n--;
+}
