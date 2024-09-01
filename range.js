a438934ab8395b4b67d3ae141155ad9ba1d34567
shoyonp <shoyonkumar143@gmail.com>
2024-09-01 23:59:51 +0600

    added range file

---
+++ range.js
@@ -0,0 +1,11 @@
+// let num = 1;
+// while (num <= 10){ // focus on the cndition
+//     console.log(num);
+//     num ++;
+// }
+
+let num = 50;
+while(num <= 100){
+    console.log(num);
+    num ++;
+}
