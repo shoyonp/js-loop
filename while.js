6d1b72e159ad377ab94ee271a2259b55442f5fcb
shoyonp <shoyonkumar143@gmail.com>
2024-09-01 23:54:00 +0600

    added while loop file

--- while.js
+++ while.js
@@ -1,5 +1,14 @@
 const number = 7;
 if(number < 10){
-    console.log('i got a number')
+    console.log('i got a number');
 }
 console.log('somthing new is cooking');
+
+//  while loop
+let num = 0; // loop variable
+while(num < 5){
+    console.log('siuuu', num);
+    // num = num + 1;
+    // num =+ 1;
+    num++;
+}
